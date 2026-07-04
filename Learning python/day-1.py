a6c84afbe02d9d9b77a7391c903b2f2105565fb4
anuragjai123 <jaiswalanurag848@gmail.com>
2026-07-04 11:55:04 +0530

    day 1 of learning python

---
+++ Learning python/day-1.py
@@ -0,0 +1,28 @@
+# date - 4 july 2026
+
+# print 
+print("india" , "five" , "true")
+
+# sep parameter - used to separate the values with a specific character
+print("india" , "five" , "true" , sep = "-")
+
+# end parameter - used to end the print statement with a specific character
+print("hello" ,end = " " )
+print("world")
+
+# data types 
+# basic data types - int , float , str , bool 
+# container data types - list , tuple , set , dict
+# user defined data types - class , object
+
+# int data type - used to store whole numbers limit - 1e309
+print(1e309)
+
+# float data type - used to store decimal numbers limit - 1e-324
+print(1.23)
+
+# bool data type - used to store true or false values
+print(True)
+
+# string data type - used to store text values
+print("hello world")
