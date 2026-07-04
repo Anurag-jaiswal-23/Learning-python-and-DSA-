21df37bb0fb3c7510e6c0a58b7b80916c33b210b
anuragjai123 <jaiswalanurag848@gmail.com>
2026-07-04 20:41:15 +0530

    keyword completer

--- Learning python/day-1.py
+++ Learning python/day-1.py
@@ -26,3 +26,46 @@ print(True)
 
 # string data type - used to store text values
 print("hello world")
+
+# comments - used to add notes to the code
+
+# variables - used to store data in memory 
+
+name = "anurag"
+print(name)
+name = 4 
+print(name)
+
+# dynamic typing - python is a dynamically typed language which means that we don't need to declare the data type of a variable while declaring it. The data type of a variable is determined at runtime based on the value assigned to it.
+
+# static typing - in static typing languages we need to declare the data type of a variable while declaring it. The data type of a variable is determined at compile time based on the value assigned to it.
+
+# dynamic binding - in dynamic binding languages the data type of a variable can be changed at runtime based on the value assigned to it.
+
+# static binding - in static binding languages the data type of a variable cannot be changed at runtime based on the value assigned to it.
+
+# runtime - the time during which a program is running
+# compile time - the time during which a program is being compiled 
+# compiler - a program that converts source code into machine code
+# interpreter - a program that executes source code line by line
+
+# special syntax 
+
+a = 5;b = 10;c = 15
+print(a,b,c)
+
+a ,b,c = 5,10,15
+print(a,b,c)
+
+# keywords - reserved words in python which cannot be used as variable names
+
+import keyword
+print(keyword.kwlist)
+
+# ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+# identifiers - names given to variables, functions, classes, etc. in python
+# rules for identifiers -
+# 1. can contain letters, digits and underscores  
+# 2. cannot start with a digit
+# 3. cannot be a keyword
